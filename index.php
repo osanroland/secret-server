69cf3183284a077bdf02c0c7b7ecc660ecb0b6cc
osanroland <osanroland17@gmail.com>
2022-09-27 06:11:33 +0200

    composer added

--- index.php
+++ index.php
@@ -2,14 +2,7 @@
 require __DIR__ . "/inc/bootstrap.php";
  
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-// $uri = explode( '/', $uri );
- 
-/*if ((isset($uri[2]) && $uri[2] != 'user') || !isset($uri[3])) {
-    header("HTTP/1.1 404 Not Found");
-    exit();
-}*/
 
- 
 require PROJECT_ROOT_PATH . "/Controller/Api/SecretController.php";
  
 $controller = new SecretController();
